7d79950afb6913655f83596c589a33bdeb995f5b
Kenneth Lee <kennethslee.613@gmail.com>
2020-02-23 18:16:04 -0500

    started blog content method in blog.service

---
+++ src/app/models/BlogContent.ts
@@ -0,0 +1,4 @@
+export interface BlogContent {
+  id: number;
+  content: string;
+}
--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -1,24 +1,42 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Blog } from '../models/Blog';
+import { BlogCard } from '../models/BlogCard';
 import { throwError, Observable } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { BlogContent } from '../models/BlogContent';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  private _url: string = "/assets/data/blogs.json";
+  private _blogsUrl: string = "/assets/data/blogs.json";
+  private _blogContentsUrl: string = "/assets/data/blogContents.json";
 
   constructor(private http: HttpClient) { }
 
-  getBlogs(): Observable<Blog[]> {
-    return this.http.get<Blog[]>(this._url).pipe(
+  getBlogCards(): Observable<BlogCard[]> {
+    return this.http.get<BlogCard[]>(this._blogsUrl).pipe(
       retry(2),
       catchError(this.errorHandler)
     );
   }
+
+  // Once backend and api is set up
+
+  // getBlogCard(id): Observable<BlogCard> {
+  //   return this.http.get<BlogCard[]>(this._blogsUrl).pipe(
+  //     retry(2),
+  //     catchError(this.errorHandler)
+  //   );
+  // }
+
+  // getBlogContent(id): Observable<BlogContent> {
+  //   return this.http.get<BlogContent[]>(this._blogContentsUrl).pipe(
+  //     retry(2),
+  //     catchError(this.errorHandler)
+  //   );
+  // }
 
   errorHandler(error: HttpErrorResponse) {
     return throwError(error.message || "Internal Server Error");
